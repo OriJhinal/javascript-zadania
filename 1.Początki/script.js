db678d06a86f781dd8105c57300bd8044f79f42a
OriJhinal <63016999+OriJhinal@users.noreply.github.com>
2021-02-06 16:47:31 +0100

    Update script.js

--- 1.Początki/script.js
+++ 1.Początki/script.js
@@ -1,3 +1,14 @@
-Zad 1
+//Zad 1
 
 console.log("Twoje Imię");  //w konsoli wyświetlił się ten napis
+
+//Zad 2
+12 + 32 = 44
+52/16 = 3.25
+5-2 = 10
+
+//Zad 3
+
+"Hello world" //nic
+Hello world //Błąd - nieokreślona wartość/typ
+"Hello world //Błąd - brak drugiego nawiasu
