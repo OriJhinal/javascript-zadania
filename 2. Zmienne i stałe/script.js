fc3b0123218e44416950c9c5f2dec89cf01dcf55
OriJhinal <63016999+OriJhinal@users.noreply.github.com>
2021-02-06 17:02:08 +0100

    Update script.js

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,28 @@
+//Zad 1
+
+const num = 1,
+    str = '2',
+    combo = num + str,
+    bool = true,
+    nothing = null;
+
+console.log(num);
+console.log(str);
+console.log(combo);
+console.log(bool);
+console.log(nothing);
+
+//Zad 2
+
+let num = 1,
+    num1 = 2,
+    sum = 0;
+
+sum = num + num1;
+
+console.log(sum);
+
+//Zad 3
+
+let num;
+console.log(num); // brak przypisanej wartości do zmiennej
